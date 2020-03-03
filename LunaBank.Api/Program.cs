922dbaf315dcc7beb99383e7eeebc49850e67026
Kabiru Okeleye <kabiruokeleye@gmail.com>
2020-03-03 17:34:12 +0100

    project set-up completed

--- LunaBank.Api/Program.cs
+++ LunaBank.Api/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lunabank.Data.Models;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,8 +18,10 @@ namespace LunaBank.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+             CreateHostBuilder(args).Build().Run();
         }
+      
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
---
+++ Lunabank.Data/Models/Transactions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lunabank.Data.Models
+{
+    public partial class Transactions
+    {
+        [Key]
+        public Guid TransactionId { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedOn { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TransactionType { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string AccountNumber { get; set; }
+        [Required]
+        [Column("cashier")]
+        [StringLength(128)]
+        public string Cashier { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal Amount { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal OldBalance { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal NewBalance { get; set; }
+    }
+}
