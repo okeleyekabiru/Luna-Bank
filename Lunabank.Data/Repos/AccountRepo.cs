b9198854834fecdd2548f82de7d0f7c1a8434444
Kabiru Okeleye <kabiruokeleye@gmail.com>
2020-03-04 16:38:30 +0100

    project setup updated

--- Lunabank.Data/Repos/AccountRepo.cs
+++ Lunabank.Data/Repos/AccountRepo.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Lunabank.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lunabank.Data.Repos
 {
   public  class AccountRepo:IAccounRepo
     {
+        private readonly DataContext _context;
+     
+
+      
+
+      
     }
 }
--- Lunabank.Data/Repos/IAccounRepo.cs
+++ Lunabank.Data/Repos/IAccounRepo.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Lunabank.Data.Entities;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lunabank.Data.Repos
 {
     public interface IAccounRepo
     {
+      
+      
+
     }
 }
