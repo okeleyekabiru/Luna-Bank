e777c5fd1dd579ac5d53aefd3a4fce7fece98050
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-03-05 13:28:55 +0100

    commit after rebase

---
+++ Lunabank.Data/Models/ApplicationSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunabank.Data.Models
+{
+    public class ApplicationSettings
+    {
+        public string JWT_Secret { get; set; }
+    }
+}
---
+++ Lunabank.Data/Repos/UserRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lunabank.Data.Entities;
+
+namespace Lunabank.Data.Repos
+{
+    public class UserRepo : IUserRepo
+    {
+        private readonly DataContext _context;
+
+        public UserRepo(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public bool EmailExist(string email)
+        {
+            return _context.Users.Any(c => c.Email == email);
+        }
+    }
+}
