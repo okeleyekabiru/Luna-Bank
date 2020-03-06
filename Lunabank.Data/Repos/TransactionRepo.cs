577ee733477ba7876a80da6056588d1b3be2e0f0
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-06 23:38:26 +0100

    implement create credit transaction

--- Lunabank.Data/Repos/TransactionRepo.cs
+++ Lunabank.Data/Repos/TransactionRepo.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Lunabank.Data.Entities;
 
 namespace Lunabank.Data.Repos
 {
   public  class TransactionRepo:ITransactionRepo
     {
+        private readonly DataContext _context;
+
+        public TransactionRepo(DataContext context)
+        {
+            _context = context;
+        }
+        public void create(Transactions transaction)
+        {
+            transaction.TransactionId = new Guid();
+            _context.Transactions.Add(transaction);
+        }
     }
 }
