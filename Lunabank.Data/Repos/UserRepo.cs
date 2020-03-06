9f1fed2e849f0e404a5ac51ff8b7508269bd0c9f
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-06 15:16:37 +0100

    implement get current userId feature

--- Lunabank.Data/Repos/UserRepo.cs
+++ Lunabank.Data/Repos/UserRepo.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using Lunabank.Data.Entities;
+using Microsoft.AspNetCore.Http;
 
 namespace Lunabank.Data.Repos
 {
     public class UserRepo : IUserRepo
     {
         private readonly DataContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public UserRepo(DataContext context)
+        public UserRepo(DataContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
+            _httpContextAccessor = httpContextAccessor;
         }
         public bool EmailExist(string email)
         {
-            return _context.Users.Any(c => c.Email == email);
+            return _context.Users.Any(c => c.Email.Equals(email, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public string GetCurrentUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext.User?.Claims?
+                .FirstOrDefault(u => u.Type == "UserID").Value;
+            return userId;
         }
     }
 }
