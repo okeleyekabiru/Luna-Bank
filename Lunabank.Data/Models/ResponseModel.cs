657534c0a016eeaf494b2b2ab167b8912da2eed9
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-07 12:17:57 +0100

    add response model class

---
+++ Lunabank.Data/Models/ResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunabank.Data.Models
+{
+    public class ResponseModel<T>
+    {
+        public string Status { get; set; }
+        public T Data { get; set; }
+    }
+}
