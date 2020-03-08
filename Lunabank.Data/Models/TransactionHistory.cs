ed1cac5df5d1e41b1fe53c72fc27b913cdd69184
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-08 14:25:38 +0100

    add TransactionHistory class

---
+++ Lunabank.Data/Models/TransactionHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunabank.Data.Models
+{
+    public class TransactionHistory
+    {
+        private const int maxPageSize = 10;
+        private int _pageSize = 10;
+        public int PageNumber { get; set; } = 1;
+        public DateTime? FromDate { get; set; }
+
+        public int Pagesize
+        {
+            get => _pageSize;
+            set => _pageSize = value > maxPageSize ? maxPageSize : value;
+        }
+    }
+}
