16f6f8f4a5e77d8b5dc74409c130ba410f6d4a54
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-03-28 17:48:09 +0100

    Abdkfkdc

--- LunaUi/src/app/app.module.ts
+++ LunaUi/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from 'src/NavBar/NavbarComponet';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms'
+import { ApppageComponent } from './Apppage.component';
+import { FooterComponent } from './Footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ApppageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component:ApppageComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
