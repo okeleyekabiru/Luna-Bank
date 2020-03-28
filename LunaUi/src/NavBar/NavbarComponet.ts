f600aecd2b71f1314d84993e93fefe9348712248
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-03-28 17:33:52 +0100

    Add navbar

---
+++ LunaUi/src/NavBar/NavbarComponet.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./NavbarComponent.html",
+    selector:"pm-navbar"
+})
+export class NavbarComponent{
+    Search: string;
+}
