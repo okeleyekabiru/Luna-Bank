7df5148426374c369607e0bcc8ecce7a4d790f2c
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-03-30 14:30:30 +0100

    Add AllAccount Interface

---
+++ LunaUi/src/app/interface/IAllAccount.ts
@@ -0,0 +1,13 @@
+export interface IAllAccount{
+  
+        accountId:string
+        accountNumber: string,
+        createdOn: Date,
+        accountType: string,
+        status: string,
+        balance: number,
+        firstName: string,
+        lastName: string
+    
+      
+}
