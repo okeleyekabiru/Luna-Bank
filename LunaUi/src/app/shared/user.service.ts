288dfb218171c45a5dab6753864cc682412e316f
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-03-30 19:13:34 +0100

    add register service

--- LunaUi/src/app/shared/user.service.ts
+++ LunaUi/src/app/shared/user.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
+  constructor(private fb: FormBuilder, private http: HttpClient ) {}
+  readonly BaseURI = "http://localhost:5000/api";
 
-  constructor() { }
+  formModel = this.fb.group({
+    FirstName: ["", Validators.required],
+    LastName: ["", Validators.required],
+    Email: ["", Validators.email],
+    Passwords: this.fb.group(
+      {
+        Password: ["", [Validators.required, Validators.minLength(4)]],
+        ConfirmPassword: ["", Validators.required]
+      },
+      { validator: this.comparePasswords }
+    )
+  });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get("ConfirmPassword");
+    //passwordMismatch
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (
+      confirmPswrdCtrl.errors == null ||
+      "passwordMismatch" in confirmPswrdCtrl.errors
+    ) {
+      if (fb.get("Password").value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else confirmPswrdCtrl.setErrors(null);
+    }
+  }
+  register() {
+    var body = {
+      FirstName: this.formModel.value.FirstName,
+      LastName: this.formModel.value.LastName,
+      Email: this.formModel.value.Email,
+      Password: this.formModel.value.Passwords.Password
+    };
+    return this.http.post(this.BaseURI + "/User/Register", body);
+  }
 }
