76541e3a5c948d969d712195f24926f4dfc14ce2
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-03-31 21:19:57 +0100

    add logout function

--- LunaUi/src/NavBar/NavbarComponet.ts
+++ LunaUi/src/NavBar/NavbarComponet.ts
@@ -1,9 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: "./NavbarComponent.html",
-    selector:"pm-navbar"
+  templateUrl: "./NavbarComponent.html",
+  selector: "pm-navbar"
 })
-export class NavbarComponent{
-    Search: string;
+export class NavbarComponent implements OnInit {
+  Search: string;
+    shouldLogOut = false;
+    
+    constructor(private router: Router) { }
+    
+    ngOnInit() {
+        if (localStorage.getItem("access_token") != null) {
+            this.shouldLogOut = true;
+        }
+    }
+    
+  onLogout() {
+    localStorage.removeItem("access_token");
+    this.router.navigate(["/user/login"]);
+  }
 }
